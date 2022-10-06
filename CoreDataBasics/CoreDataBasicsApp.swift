267e13e94ab6ba6828bd5d7f487b6abbf9f6ba43
hugosilvag6 <hugosilvag6@hotmail.com>
2022-10-06 16:44:26 -0300

    Initial Commit

---
+++ CoreDataBasics/CoreDataBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBasicsApp.swift
+//  CoreDataBasics
+//
+//  Created by Hugo Silva on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
